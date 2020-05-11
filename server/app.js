b08a6e2155ea89d3eb5d5dc0f5c5a04d93d6b52c
hothdung <ho.thdung@gmail.com>
2020-05-11 17:21:14 +0900

    receiving the signals and insert into databases

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 var cors = require('cors');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
-var http = require('http');
 var app = express();
 
 
@@ -11,8 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-
-// connection to interventions_db
 
 var connection = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -22,17 +19,13 @@ var connection = mysql.createConnection({
     port: process.env.DB_PORT
 })
 
-// connections to signals_db
-
 var signalsConnection = mysql.createConnection({
     host: process.env.DB_SIG_HOST,
     user: process.env.DB_SIG_USER,
     password: process.env.DB_SIG_PASS,
     database: process.env.DB_SIG_DATABASE,
     port: process.env.DB_SIG_PORT
-});
-
-
+})
 
 app.post('/interventions', function (req, res) {
     const intervention = {
@@ -67,67 +60,73 @@ app.get('/posts', function (req, res) {
     });
 })
 
-// app.post('/addSignal', function (req, res) {
-
-//     console.log("Hello from Here" + req);
-//     var signalType = req.body.body
-//     var dataObj = {};
-//     var q;
-//     if (signalType === "heartrate") {
-//         q = "INSERT INTO heartrate SET ?;";
-
-//         dataObj = {
-//             bpm: req.body.bpm,
-//             date: req.body.date,
-//             studentId: req.body.studentId
-//         }
-//         console.log(req.body.signalType);
-//         console.log("Here is the data Object " + dataObj);
-//     } else if (signalType === "locations") {
-//         dataObj = {
-//             long: req.body.long,
-//             lat: req.body.lat,
-//             date: req.body.date,
-//             studentId: req.body.studentId
-//         }
-//     }
-//     else if (signalType === "movements") {
-//         dataObj = {
-//             gravity: req.body.gravity,
-//             acceleration: req.body.acceleration,
-//             rotation: req.body.rotation,
-//             attitude: req.body.attitude,
-//             date: req.body.date,
-//             studentId: req.body.studentId
-//         }
-//     }
-//     else if (signalType === "audio") {
-//         dataObj = {
-//             audio: req.body.audio,
-//             date: req.body.date,
-//             studentId: req.body.studentId
-//         }
-//     }
-//     else {
-//         console.log("wrong signal type!")
-//     }
-
-//     signalsConnection.query(q, dataObj, function (error, result) {
-//         if (error) throw error;
-//         console.log(result);
-//     })
-//     res.status(200).end();
-//     console.log("Signal successfully added to the database");
-//     console.log("POST Request SENT To /addSignal");
-
-// }
-// )
-
 app.post('/addSignal', function (req, res) {
-    res.status(200).send('Hit successfully')
-    console.log("received the test message");
-    console.log(req.body);
+    var signalType = req.body.signalType;
+    var dataObj = {};
+    var q;
+    var tableSpec;
+    if (signalType === "heartrate") {
+        dataObj = {
+            bpm: req.body.bpm,
+            date: req.body.date,
+            studentId: req.body.studentId
+        }
+        q = "INSERT INTO heartrate SET ?;"
+        tableSpec = "heartrate table"
+    }
+    else if (signalType === "locations") {
+        dataObj = {
+            long: req.body.long,
+            lat: req.body.lat,
+            date: req.body.date,
+            studentId: req.body.studentId
+        }
+        q = "INSERT INTO locations SET ?;"
+        tableSpec = "locations table"
+    }
+
+    else if (signalType === "movements") {
+        dataObj = {
+            gravity: req.body.gravity,
+            acceleration: req.body.acceleration,
+            rotation: req.body.rotation,
+            attitude: req.body.attitude,
+            fallenDown: req.body.fallenDown,
+            date: req.body.date,
+            studentId: req.body.studentId
+        }
+        q = "INSERT INTO movements SET ?;"
+        tableSpec = "movements table"
+    }
+    else if (signalType === "manual") {
+        dataObj = {
+            long: req.body.long,
+            lat: req.body.lat,
+            bpm: req.body.bpm,
+            gravity: req.body.gravity,
+            acceleration: req.body.acceleration,
+            rotation: req.body.rotation,
+            attitude: req.body.attitude,
+            fallenDown: req.body.fallenDown,
+            message: req.body.message,
+            date: req.body.date,
+            studentId: req.body.studentId
+        }
+        q = "INSERT INTO manual SET ?;"
+        tableSpec = "manual table"
+    }
+    else {
+        return new Error('Wrong signal type!')
+    }
+    signalsConnection.query(q, dataObj, function (error, result) {
+        if (error) throw error;
+        console.log(result);
+        res.send("Posted to DB");
+        console.log("Posted to " + tableSpec);
+    })
 })
+
+
 
 app.listen(8080, function () {
     console.log("Server is listening on port 8080!");
